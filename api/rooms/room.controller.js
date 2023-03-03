7bf7ef226e1bb5857af7951b21b71a07347c60e1
JayCabasag99 <jay@metromart.com>
2023-03-03 22:20:28 +0800

    Added new chanegs

--- api/rooms/room.controller.js
+++ api/rooms/room.controller.js
@@ -180,6 +180,26 @@ module.exports = {
         })
     },
 
+    userTemperatureHistory: (req, res) => {
+        const id = req.params.id
+        userVisitedRooms({user_id: id}, (err, results ) => {
+            if(err){
+                console.log(err)
+                return res.json({
+                    success: 0,
+                    message: "Database connection Error"
+                });
+            }
+
+            return res.status(200).json({
+                success: 1,
+                data: results
+            });
+            
+
+        })
+    },
+
     userTodaysTemperature: (req, res) => {
         
         const body = req.body;
--- api/rooms/room.router.js
+++ api/rooms/room.router.js
@@ -1,4 +1,4 @@
-const {addRoom, getAllRooms, addVisitedRoom, searchRoomNumber, userVisitedRooms, addUserNotification, userTodaysTemperature, searchUsersByRoomId} = require("./room.controller")
+const {addRoom, getAllRooms, addVisitedRoom, searchRoomNumber, userVisitedRooms, addUserNotification, userTodaysTemperature, searchUsersByRoomId, userTemperatureHistory} = require("./room.controller")
 const router = require("express").Router();
 const { checkToken } = require("../../auth/token_validator")
 
@@ -8,7 +8,7 @@ router.post("/addVisitedRoom", checkToken, addVisitedRoom)
 router.post("/searchRoom", checkToken, searchRoomNumber)
 router.post("/searchUsersByRoomId", checkToken, searchUsersByRoomId)
 router.post("/userVisitedRooms", checkToken, userVisitedRooms)
-router.get("/temperature-history/:id", checkToken, userVisitedRooms)
+router.get("/temperature-history/:id", checkToken, userTemperatureHistory)
 router.post("/userTodaysTemperature", checkToken, userTodaysTemperature)
 
 searchUsersByRoomId
